42aa851e2b14fdf511dcc73d8cfbbc6619e38e50
JustFiesta <mateusz.bocak9@gmail.com>
2025-10-23 16:05:18 +0200

    Enable admin access for creator (simpler than access policies)

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -3,6 +3,7 @@ module "eks" {
   version = "~> 21.0"
 
   endpoint_public_access = var.public_endpoint
+  enable_cluster_creator_admin_permissions = true
 
   name               = var.cluster_name
   kubernetes_version = var.k8s_version
@@ -40,20 +41,6 @@ module "eks" {
   }
 
   tags = var.tags
-
-  access_entries = {
-    cluster-admin = {
-      principal_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/mbocak"
-      policy_associations = {
-        admin-policy = {
-          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-          access_scope = {
-            type = "cluster"
-          }
-        }
-      }
-    }
-  }
 }
 
 
