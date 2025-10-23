79ffc78567b71772fece4b7b9176129e8ec947bc
JustFiesta <mateusz.bocak9@gmail.com>
2025-10-23 23:49:56 +0200

    Do not create any EKS IAM/OIDC

--- terraform/modules/eks/outputs.tf
+++ terraform/modules/eks/outputs.tf
@@ -6,8 +6,8 @@ output "cluster_endpoint" {
     value = module.eks.cluster_endpoint
 }
 
-output "kubeconfig-certificate-authority-data" {
-    value = module.eks.cluster_certificate_authority_data
+output "cluster_arn" {
+    value = module.eks.cluster_arn
 }
 
 output "cluster_name" {
