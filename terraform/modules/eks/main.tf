d3f9a0f3a930bb1e0727aea73a088632db2fd3d2
JustFiesta <mateusz.bocak9@gmail.com>
2025-11-05 15:32:23 +0100

    Update Terraform EKS with addons (EBS, DNS, Cluster networking, etc.)

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -59,7 +59,54 @@ resource "aws_iam_role_policy_attachment" "node_policies" {
   policy_arn = each.value
 }
 
+resource "aws_iam_role" "ebs_csi_driver" {
+  name                 = "${var.cluster_name}-ebs-csi-driver-role"
+  permissions_boundary = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:policy/DefaultBoundaryPolicy"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "pods.eks.amazonaws.com"
+      }
+      Action = [
+        "sts:AssumeRole",
+        "sts:TagSession"
+      ]
+    }]
+  })
+
+  tags = merge(
+    var.tags,
+    {
+      Name = "${var.cluster_name}-ebs-csi-driver-role"
+    }
+  )
+}
+
+resource "aws_iam_role_policy_attachment" "ebs_csi_driver_policy" {
+  role       = aws_iam_role.ebs_csi_driver.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
+
+
+resource "aws_eks_pod_identity_association" "ebs_csi_driver" {
+  cluster_name    = module.eks.cluster_name
+  namespace       = "kube-system"
+  service_account = "ebs-csi-controller-sa"
+  role_arn        = aws_iam_role.ebs_csi_driver.arn
+
+  tags = var.tags
+
+#   depends_on = [
+#     module.eks.cluster_addons
+#   ]
+}
+
 module "eks" {
+#   depends_on = [ aws_eks_pod_identity_association.ebs_csi_driver ]
+
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 21.0"
 
@@ -71,22 +118,40 @@ module "eks" {
 
   vpc_id     = var.vpc_id
   subnet_ids = var.private_subnets
-#  subnet_ids = concat(var.public_subnets, var.private_subnets)
 
+  iam_role_arn                       = aws_iam_role.cluster.arn
   iam_role_permissions_boundary      = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:policy/DefaultBoundaryPolicy"
   node_iam_role_permissions_boundary = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:policy/DefaultBoundaryPolicy"
 
   enable_irsa = false
-#   create_iam_role = false
-#   create_node_iam_role = false
-#   create_auto_mode_iam_resources = false
-
-  iam_role_arn    = aws_iam_role.cluster.arn
 
   compute_config = {
     enabled       = true
     node_pools    = ["general-purpose"]
     node_role_arn = aws_iam_role.node.arn
+  }
+
+  addons = {
+    # VPC CNI - pod networking
+    vpc-cni = {
+      most_recent = true
+    }
+
+    # CoreDNS - cluster DNS
+    coredns = {
+      most_recent = true
+    }
+
+    # kube-proxy - service networking
+    kube-proxy = {
+      most_recent = true
+    }
+
+    # EBS CSI Driver - for persistent volumes
+    # Pod Identity instead IRSA
+    aws-ebs-csi-driver = {
+      most_recent = true
+    }
   }
 
   tags = var.tags
